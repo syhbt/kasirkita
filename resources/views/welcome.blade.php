d136b4edf8d52ae17a3bc0d09c2fc6e7126381ea
Rama <syahbita.rama@gmail.com>
2021-04-15 11:17:30 +0700

    KasirKita first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kasir Kita</title>
+</head>
+<body bgcolor="white">
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<center><h1>KasirKita Under Construction</h1></center>
+</body>
+</html>
